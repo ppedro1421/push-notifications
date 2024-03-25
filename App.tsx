2c642b9310774835caaca1f5eed7a37feb4d9f4f
ppedro <ppedro.o.verissimo@gmail.com>
2024-03-25 08:47:19 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { usePushNotifications } from './src/hooks/usePushNotifications';
 
 export default function App() {
+  const { expoPushToken } = usePushNotifications();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontWeight: 'bold' }}>Token</Text>
+      <Text style={{ fontSize: 12 }}>{expoPushToken?.data}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 25,
   },
 });
