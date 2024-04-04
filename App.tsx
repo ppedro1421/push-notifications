2b75ef5a2bb0dddc0bd765218ce4893b583f870f
ppedro <ppedro.o.verissimo@gmail.com>
2024-04-04 12:17:56 -0300

    feat: reanimated

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import * as Clipboard from 'expo-clipboard';
+import Animated, { Easing, ReduceMotion, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { usePushNotifications } from './src/hooks/usePushNotifications';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const { expoPushToken } = usePushNotifications();
+
+  const translateY = useSharedValue(600);
+
+  const reanimatedCard = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    };
+  }, []);
+
+  useEffect(() => {
+    translateY.value = withTiming(0, { duration: 1000, easing: Easing.inOut(Easing.quad), reduceMotion: ReduceMotion.System })
+  }, []);
 
   const copyToClipboard = async () => {
     if (expoPushToken?.data) {
@@ -12,24 +28,24 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <LinearGradient style={styles.container} colors={['#663388', '#443388']}>
+      <StatusBar style='light' />
       <View style={styles.banner}>
 
       </View>
-      <View style={styles.card}>
+      <Animated.View style={[styles.card, reanimatedCard]}>
         <Text style={styles.title}>Push Notifications</Text>
         <TouchableOpacity style={styles.button} onPress={copyToClipboard}>
-          <Text style={styles.buttonText}>Get Token</Text>
+          <Text style={styles.buttonText}>Copy Token</Text>
         </TouchableOpacity>
-      </View>
-    </View>
+      </Animated.View>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#A751C5',
   },
   banner: {
     flex: 1,
@@ -46,11 +62,11 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 28,
     fontWeight: 'bold',
-    color: '#888888',
+    color: '#666666',
   },
   button: {
     padding: '5%',
-    backgroundColor: '#A751C5',
+    backgroundColor: '#663388',
     borderRadius: 10,
   },
   buttonText: {
